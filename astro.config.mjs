22b0e56a9637d5eeb5267fc82de6b05a004b6d95
unknown <Boom05@windowslive.com>
2025-09-03 20:56:39 +0700

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://1nky3.github.io',
+  base: '/the-work-of-tanawat/',
+});
